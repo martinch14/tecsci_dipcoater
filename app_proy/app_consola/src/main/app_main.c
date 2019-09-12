239769377f7afb2183b689d98746950649e3d45c
Martin <martinch14@gmail.com>
2019-09-12 17:46:42 -0300

    test con tynish + módulo de mensajería

--- app_proy/app_consola/src/main/app_main.c
+++ app_proy/app_consola/src/main/app_main.c
@@ -17,10 +17,14 @@ __CLEANSTIN getchar():
 
 static void commandHolahanlder(int argc, char **argv){
 	if(4 <= argc){
-//		char* name = tinysh_get_arg_string(argc, argv, 0);
-//		int		entero 	= tinysh_get_arg_int(argc, argv, 1);
-//		float	decimal = tinysh_get_arg_float(argc, argv, 2);
-//		char*	texto 	= tinysh_get_arg_string(argc, argv, 3);
+		char* name = tinysh_get_arg_string(argc, argv, 0);
+		int		entero 	= tinysh_get_arg_int(argc, argv, 1);
+		float	decimal = tinysh_get_arg_float(argc, argv, 2);
+		char*	texto 	= tinysh_get_arg_string(argc, argv, 3);
+		char*	texto2 	= tinysh_get_arg_string(argc, argv, 4);
+		char*	texto3 	= tinysh_get_arg_string(argc, argv, 5);
+		char*	texto4 	= tinysh_get_arg_string(argc, argv, 6);
+
 
 //		api_print("asd %d");
 //		printf("asd\0");
@@ -28,7 +32,13 @@ static void commandHolahanlder(int argc, char **argv){
 	}
 }
 
-static tinysh_cmd_t commandHola = {NULL, "hola", NULL, NULL, commandHolahanlder, NULL, NULL, NULL};
+static tinysh_cmd_t commandDOWN = {NULL,"DOWN"				, NULL, NULL, commandHolahanlder, NULL, NULL, NULL};
+static tinysh_cmd_t commandUP = {NULL,"UP"					, NULL, NULL, commandHolahanlder, NULL, NULL, NULL};
+static tinysh_cmd_t commandSPIN = {NULL,"SPIN"				, NULL, NULL, commandHolahanlder, NULL, NULL, NULL};
+static tinysh_cmd_t commandWAIT = {NULL,"WAIT"				, NULL, NULL, commandHolahanlder, NULL, NULL, NULL};
+static tinysh_cmd_t commandSTOP = {NULL,"STOP"				, NULL, NULL, commandHolahanlder, NULL, NULL, NULL};
+static tinysh_cmd_t commandCEROMACHINE = {NULL,"CEROMACHINE", NULL, NULL, commandHolahanlder, NULL, NULL, NULL};
+
 
 void HandlerConsolePutchar(unsigned char c){
 	putchar(c);
@@ -44,13 +54,23 @@ typedef struct{
 }app_cfg;
 
 int app_main(void) {
-	tinysh_add_command(&commandHola);
+
+	tinysh_add_command(&commandDOWN);
+	tinysh_add_command(&commandUP);
+	tinysh_add_command(&commandSPIN);
+	tinysh_add_command(&commandWAIT);
+	tinysh_add_command(&commandSTOP);
+	tinysh_add_command(&commandCEROMACHINE);
+
 	tinysh_set_putchar(HandlerConsolePutchar);
+
 	tinysh_init();
 	char c;
 	while(1){
 		c = getchar();
 		tinysh_char_in(c);
+		printf(ddsds->name);
+
 	}
 	return 0;
 }
