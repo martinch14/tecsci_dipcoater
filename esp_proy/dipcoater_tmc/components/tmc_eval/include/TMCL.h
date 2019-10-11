95ed4633ff18d342b05aaa76238ae9a8c7de2144
seb <seb@ilm>
2019-10-11 00:45:54 -0300

    Porting de TMC a ESP
    Los accesos a hardware fueron reemplazados por printf.
    Tuve que realizar varias definiciones para el proting, el código de TMC
    era  muy dependiente de la sapi.

---
+++ esp_proy/dipcoater_tmc/components/tmc_eval/include/TMCL.h
@@ -0,0 +1,14 @@
+#ifndef TMCL_H
+#define TMCL_H
+
+#include <stddef.h>
+#include "API_Header.h"
+#include "Board.h"
+#include "defs.h"
+#include "main.h"
+
+void tmcl_init();
+void tmcl_process();
+void tmcl_boot();
+
+#endif /* TMCL_H */
